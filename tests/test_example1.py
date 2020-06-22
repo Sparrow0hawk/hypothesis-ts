bde0742df717a0cddb9ac1eb1c23735021a60274
sparrow0hawk <alexjcoleman@gmail.com>
2020-06-22 20:16:04 +0100

    given and example keyword arguements

--- tests/test_example1.py
+++ tests/test_example1.py
@@ -2,8 +2,8 @@ from code import example1
 from hypothesis import given, example
 from hypothesis.strategies import text
 
-@given(text())
+@given(s=text())
 # this ensures this examples always runs
-@example("")
+@example(s="")
 def test_decode_inverts_encode(s):
 	assert example1.decode(example1.encode(s)) == s
